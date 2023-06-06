bbabb1c8adc19aeae1d82dad8d3f6178bb9c2951
Adriel <49051133+izuca@users.noreply.github.com>
2023-06-06 01:58:00 +0000

    Update e Delete

--- Controllers/Todo.js
+++ Controllers/Todo.js
@@ -36,18 +36,38 @@ router.post('/', isLoggedIn, async (req,res) => {
             .then(log.green("DATABASE STATUS","New Document"))
             .catch((error) => {
                 res.status(400).json({ error })
-            }),
+            })
      )
      
 })
 
-// router.update('/:id', isLoggedIn, async(req,res) => {
-//     //checar se a task existe
-//     const taskId = Todo.findById(req.headers.id)
-
-// })
 // Update
-// Delete
+router.put('/:id', isLoggedIn, async(req,res) => {
+    const { username } = req.user
+    req.body.username = username
+    
+    res.json(
+        await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(log.green("DATABASE STATUS","Document Updated"))
+        .catch((error) => {
+            res.status(400).json({ error })
+        })
+    
+    )
+})
 
+// Delete
+router.delete('/:id', isLoggedIn, async(req,res) => {
+    const { username } = req.user
+    req.body.username = username
+
+    res.json(
+        await Todo.findByIdAndDelete(req.params.id)
+        .then(log.yellow("DATABASE STATUS","Document Deleted"))
+        .catch((error) => {
+            res.status(400).json({ error })
+        })
+    )
+})
 
 module.exports = router
